9c478a20d50398cba2ced4f3e9d677be5dcd8652
Didero <Didero@users.noreply.github.com>
2025-01-06 19:20:17 +0100

    Update version number in other places too

--- ModAssemblyInfo/ModAssemblyInfo.cs
+++ ModAssemblyInfo/ModAssemblyInfo.cs
@@ -10,7 +10,7 @@ namespace SpeechFreeRadio
     public class ModAssemblyInfo
     {
         public const string Name        = "SpeechFreeRadio";
-        public const string Version     = "1.0.0";
+        public const string Version     = "1.0.1";
         public const string Title       = "Speech Free Radio";
         public const string Description = "Disable all talking on the in-game radio";
     }
